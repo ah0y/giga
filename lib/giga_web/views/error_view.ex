a33cffe75fb5cce6eca1c4c4b15ec4d249b5d66e
Aberham Haile <aberhamhaile@Aberhams-MacBook-Pro.local>
2020-11-15 11:32:49 -0500

    rearramge error view order

--- lib/giga_web/views/error_view.ex
+++ lib/giga_web/views/error_view.ex
@@ -1,19 +1,6 @@
 defmodule GigaWeb.ErrorView do
   use GigaWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-  def template_not_found(template, _assigns) do
-    Phoenix.Controller.status_message_from_template(template)
-  end
-
   def render("500.html", _assigns) do
     case Sentry.get_last_event_id_and_source() do
       {event_id, :plug} when is_binary(event_id) ->
@@ -33,5 +20,12 @@ defmodule GigaWeb.ErrorView do
       _ ->
         "Internal Server Error"
     end
+  end
+
+  # By default, Phoenix returns the status message from
+  # the template name. For example, "404.html" becomes
+  # "Not Found".
+  def template_not_found(template, _assigns) do
+    Phoenix.Controller.status_message_from_template(template)
   end
 end
